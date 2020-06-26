cfa3925739e0291d573a691c7ee530303a3f3422
md11071 <66173691+md11071@users.noreply.github.com>
2020-06-26 15:43:56 +0530

    Delete Hello.js

--- Module 4/Hello.js
+++
@@ -1,9 +0,0 @@
-(function (window){
-	var helloSpeaker = {};
-
-	var greet = "Hello";
-	helloSpeaker.speak = function (name) {
-  		console.log(greet + " " + name);
-		}
-	window.helloSpeaker=helloSpeaker;
-})(window);

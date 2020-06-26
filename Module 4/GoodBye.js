07718978394860e4992e8eeb7a8a40104596fdf9
md11071 <66173691+md11071@users.noreply.github.com>
2020-06-26 15:43:44 +0530

    Delete GoodBye.js

--- Module 4/GoodBye.js
+++
@@ -1,8 +0,0 @@
-(function (window){
-	var byeSpeaker = {};
-	var bye = "Good Bye";
-	byeSpeaker.speak = function (name) {
-  console.log(bye + " " + name);
-	}
-	window.byeSpeaker=byeSpeaker;
-})(window);
